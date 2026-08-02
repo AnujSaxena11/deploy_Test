92dc8abf0ddd77774808be4e8ac1e69fa7a9daf8
AnujSaxena11 <anujsaxena337@gmail.com>
2026-08-03 02:51:46 +0530

    name added in title

--- backend/app.js
+++ backend/app.js
@@ -103,15 +103,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173'];
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
